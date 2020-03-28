36430378f24cd3bb6f6baa271fc8076af9f52733
Goran Peshanski <gpeshans@hfour.com>
2020-03-29 00:46:37 +0100

    implemented grid layout system

--- src/components/grid/index.tsx
+++ src/components/grid/index.tsx
@@ -4,17 +4,27 @@ import classNames from 'classnames';
 import './index.scss';
 
 interface ContainerProps {
+  /**
+   * A space-delimited list of class names to pass along to a child element.
+   */
   className?: string;
   children: React.ReactNode;
 }
 
 interface RowProps {
+  /**
+   * A space-delimited list of class names to pass along to a child element.
+   */
   className?: string;
   children: React.ReactNode;
 }
 
 interface ColumnProps {
   children: React.ReactNode;
+  /**
+   * A space-delimited list of class names to pass along to a child element.
+   */
+  className?: string;
   span?: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
   spanMd?: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
   spanLg?: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
@@ -23,6 +33,15 @@ interface ColumnProps {
   offsetMd?: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
   offsetLg?: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
   offsetXl?: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
+  hiddenXsUp?: boolean;
+  hiddenSmUp?: boolean;
+  hiddenMdUp?: boolean;
+  hiddenLgUp?: boolean;
+  hiddenXlUp?: boolean;
+  hiddenSmDown?: boolean;
+  hiddenMdDown?: boolean;
+  hiddenLgDown?: boolean;
+  hiddenXlDown?: boolean;
 }
 
 /**
@@ -54,24 +73,44 @@ export const Row = ({ children, className }: RowProps) => {
  */
 export const Column = ({
   children,
+  className,
   span = 12,
   spanMd = 12,
-  //   spanLg = 12,
-  //   spanXl = 12,
+  spanLg = 12,
+  spanXl = 12,
   offset = 0,
   offsetMd = 0,
-}: //   offsetLg = 0,
-//   offsetXl = 0,
-ColumnProps) => {
+  offsetLg = 0,
+  offsetXl = 0,
+  hiddenXsUp = false,
+  hiddenSmUp = false,
+  hiddenSmDown = false,
+  hiddenMdUp = false,
+  hiddenMdDown = false,
+  hiddenLgUp = false,
+  hiddenLgDown = false,
+  hiddenXlUp = false,
+  hiddenXlDown = false,
+}: ColumnProps) => {
   const classes = classNames(
     `container__col-sm-${span}`,
     `container__col-md-${spanMd}`,
-    // `container__col-lg-${spanLg}`,
-    // `container__col-xl-${spanXl}`,
+    `container__col-lg-${spanLg}`,
+    `container__col-xl-${spanXl}`,
     `container__col-sm-offset-${offset}`,
     `container__col-md-offset-${offsetMd}`,
-    // `container__col-lg-offset-${offsetLg}`,
-    // `container__col-xl-offset-${offsetXl}`,
+    `container__col-lg-offset-${offsetLg}`,
+    `container__col-xl-offset-${offsetXl}`,
+    hiddenXsUp && `hidden-xs-up`,
+    hiddenSmUp && `hidden-sm-up`,
+    hiddenSmDown && `hidden-sm-down`,
+    hiddenMdUp && `hidden-md-up`,
+    hiddenMdDown && `hidden-md-down`,
+    hiddenLgUp && `hidden-lg-up`,
+    hiddenLgDown && `hidden-lg-down`,
+    hiddenXlUp && `hidden-xl-up`,
+    hiddenXlDown && `hidden-xl-down`,
+    className,
   );
   return <div className={classes}>{children}</div>;
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,45 +1,62 @@
 import * as React from 'react';
-// import { Link } from 'gatsby';
-import { Container, ContainerFluid, Row, Column } from '../components/grid';
+import { Link } from 'gatsby';
 
+import { Container, ContainerFluid, Row, Column } from '../components/grid';
 import { Layout } from '../components/layout';
 import { SEO } from '../components/seo';
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
+    <ContainerFluid>
+      <Row>
+        <Column>
+          <h1>Hi from the home page</h1>
+        </Column>
+      </Row>
+      <Row>
+        <Column>
+          <h2>Welcome to the home page</h2>
+        </Column>
+      </Row>
+      <Row>
+        <Column>
+          <Link to="/content/1/">Go to content item 1</Link>
+        </Column>
+      </Row>
+    </ContainerFluid>
     <Container>
       <Row>
-        <Column span={12} spanMd={3}>
+        <Column span={12} spanMd={3} spanLg={2} offsetLg={1} spanXl={1} offsetXl={2} hiddenXlDown>
           .column
         </Column>
-        <Column span={12} spanMd={3}>
+        <Column span={12} spanMd={3} spanLg={2} offsetLg={1} spanXl={1} offsetXl={2} hiddenSmDown>
           .column
         </Column>
-        <Column span={12} spanMd={3}>
+        <Column span={12} spanMd={3} spanLg={2} offsetLg={1} spanXl={1} offsetXl={2} hiddenMdUp>
           .column
         </Column>
-        <Column span={12} spanMd={3}>
+        <Column span={12} spanMd={3} spanLg={2} offsetLg={1} spanXl={1} offsetXl={2} hiddenXlUp>
           .column
         </Column>
       </Row>
     </Container>
-    {/* <ContainerFluid>
+    <ContainerFluid>
       <Row>
-        <Column span={12} spanMd={3}>
+        <Column span={12} spanMd={3} spanLg={2} offsetLg={1} spanXl={1} offsetXl={2} hiddenXlDown>
           .column
         </Column>
-        <Column span={12} spanMd={3}>
+        <Column span={12} spanMd={3} spanLg={2} offsetLg={1} spanXl={1} offsetXl={2} hiddenSmDown>
           .column
         </Column>
-        <Column span={12} spanMd={3}>
+        <Column span={12} spanMd={3} spanLg={2} offsetLg={1} spanXl={1} offsetXl={2} hiddenMdUp>
           .column
         </Column>
-        <Column span={12} spanMd={3}>
+        <Column span={12} spanMd={3} spanLg={2} offsetLg={1} spanXl={1} offsetXl={2} hiddenXlUp>
           .column
         </Column>
       </Row>
-    </ContainerFluid> */}
+    </ContainerFluid>
   </Layout>
 );
 
