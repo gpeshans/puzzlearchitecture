b41bc58bf4421c99431d431cac79bf9180fb2ee5
Goran Peshanski <gpeshans@hfour.com>
2020-05-01 18:04:04 +0200

    adjusted about page

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -41,7 +41,7 @@ const About = () => {
             <Column className="pzAbout__about-section-title">About</Column>
           </Row>
           <Row>
-            <Column className="pzAbout__about-section-content">
+            <Column className="pzAbout__about-section-content" spanSm={12} spanMd={11} spanLg={11} spanXl={11}>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
               dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
               ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
@@ -66,7 +66,7 @@ const About = () => {
                 <Input id="email" type="email" onChange={onEmailChange} required={true} />
               </Column>
               <Column className="pzAbout__contact-form-row">
-                <label htmlFor="message">Email</label>
+                <label htmlFor="message">Message</label>
                 <Textarea id="message" onChange={onMessageChange} required={true} />
               </Column>
               <Column className="pzAbout__contact-form-row pzAbout__contact-form-submit-button">
