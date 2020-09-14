970b456faabe7497410e4b5ff087b596141ad817
Goran Peshanski <gpeshans@hfour.com>
2020-09-15 00:57:48 +0200

    adjusted description font size

--- src/pages/thanks/index.tsx
+++ src/pages/thanks/index.tsx
@@ -2,11 +2,11 @@ import * as React from 'react';
 
 import { Page } from '../../components/page';
 
-const NotFoundPage = () => (
+const Thanks = () => (
   <Page seoProps={{ title: 'Thank you!', path: '/thanks' }}>
     <h1>Thank you!</h1>
     <p>{`Your message has been successfully sent. We will contact you very soon!`}</p>
   </Page>
 );
 
-export default NotFoundPage;
+export default Thanks;
